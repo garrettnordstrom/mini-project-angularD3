6bd9adc501e2dc4758ba514284a64c12c695ac2e
Garrett Nordstrom <garrett.a.nordstrom@gmail.com>
2015-02-12 20:36:01 -0700

    first mini project commit

---
+++ JS/controller.js
@@ -0,0 +1,14 @@
+var app = angular.module('userProfiles');
+
+app.controller('MainController', function($scope, mainService) {
+
+  $scope.getUsers = function() {
+  	$scope.users = mainService.getUsers().then(function(users) {
+  		$scope.users = 
+  	});
+  }
+
+
+  $scope.getUsers();
+
+});
